ea27001f47e2d7b9ca88a09d1be84741575bf079
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-03-22 21:30:08 -0400

    Wire up a SC theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { theme } from './theme'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        hello
-      </div>
-    );
+      <ThemeProvider theme={theme}>
+        <Main>Hello</Main>
+      </ThemeProvider>
+    )
   }
 }
 
-export default App;
+const Main = styled.main`
+  background: ${props => props.theme.colors.main};
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+`
+
+export default App
