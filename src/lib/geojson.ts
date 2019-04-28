261e3249d6c6d132e34c88c29efefef7f3c8b6e0
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-04-28 11:30:30 -0400

    Filter out nodes without locations

--- src/lib/geojson.ts
+++ src/lib/geojson.ts
@@ -1,8 +1,10 @@
 import { CityResponse } from './metaphysics'
 
 export const cityResultsToGeoJson = (data: CityResponse) => {
-  const fairs = data.city.fairs.edges.map(edge => edge.node)
-  const fairFeatures = fairs.map(fair => ({
+  const fairsWithLocations = data.city.fairs.edges
+    .filter(edge => edge.node.location)
+    .map(edge => edge.node)
+  const fairFeatures = fairsWithLocations.map(fair => ({
     type: 'Feature',
     properties: {
       type: 'fair'
@@ -16,8 +18,10 @@ export const cityResultsToGeoJson = (data: CityResponse) => {
     }
   }))
 
-  const shows = data.city.shows.edges.map(edge => edge.node)
-  const showFeatures = shows.map(show => ({
+  const showsWithLocations = data.city.shows.edges
+    .filter(edge => edge.node.location)
+    .map(edge => edge.node)
+  const showFeatures = showsWithLocations.map(show => ({
     type: 'Feature',
     properties: {
       type: 'show'
